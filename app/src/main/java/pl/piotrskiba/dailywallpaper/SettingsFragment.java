8ac59993367943aafc85ba830964bdc5b2fe391f
PanGargamel <pskiba37@gmail.com>
2018-08-08 13:38:56 +0200

    Implemented settings for automatic wallpaper changing

--- app/src/main/java/pl/piotrskiba/dailywallpaper/SettingsFragment.java
+++ app/src/main/java/pl/piotrskiba/dailywallpaper/SettingsFragment.java
@@ -1,11 +1,68 @@
 package pl.piotrskiba.dailywallpaper;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+import java.util.List;
+
+import timber.log.Timber;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
+
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
+
+        int count = preferenceScreen.getPreferenceCount();
+
+        for(int i = 0; i < count; i++){
+            Preference preference = preferenceScreen.getPreference(i);
+            if(preference instanceof ListPreference){
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
+    }
+
+    private void setPreferenceSummary(Preference preference, String value){
+        if(preference instanceof ListPreference){
+            ListPreference listPreference = (ListPreference) preference;
+            int preferenceIndex = listPreference.findIndexOfValue(value);
+            if(preferenceIndex >= 0){
+                listPreference.setSummary(listPreference.getEntries()[preferenceIndex]);
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if(preference != null){
+            if(preference instanceof ListPreference){
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
