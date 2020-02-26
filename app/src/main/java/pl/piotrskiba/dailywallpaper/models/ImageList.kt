db4d9a7319b7f140230966423e5c6936ec2f68ef
piotrskibapl <pskiba37@gmail.com>
2020-02-26 14:01:44 +0100

    convert asynctask, fragment and activity classes to kotlin

--- app/src/main/java/pl/piotrskiba/dailywallpaper/models/ImageList.kt
+++ app/src/main/java/pl/piotrskiba/dailywallpaper/models/ImageList.kt
@@ -2,4 +2,4 @@ package pl.piotrskiba.dailywallpaper.models
 
 import java.io.Serializable
 
-class ImageList(val total: Int, val totalHits: Int, val hits: Array<Image>) : Serializable
+class ImageList(val total: Int, val totalHits: Int, val hits: Array<Image?>) : Serializable
