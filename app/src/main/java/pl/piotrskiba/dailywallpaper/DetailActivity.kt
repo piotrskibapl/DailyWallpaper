79c0576ccd414ab568d6b3cb7dc84900038c6b47
piotrskibapl <pskiba37@gmail.com>
2020-03-03 08:47:15 +0100

    fixed menu items not being shown as actions

--- app/src/main/java/pl/piotrskiba/dailywallpaper/DetailActivity.kt
+++ app/src/main/java/pl/piotrskiba/dailywallpaper/DetailActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.AccelerateInterpolator
@@ -155,7 +154,7 @@ class DetailActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = MenuInflater(this)
+        val inflater = menuInflater
         inflater.inflate(R.menu.menu_detail, menu)
         return true
     }
